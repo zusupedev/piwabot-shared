3ab627319a4198bdf28d7d21c4ea924562e7f4cf
Jiniux <alessandro.gugliotta.02@gmail.com>
2020-09-08 19:13:16 +0200

    Test fix for bug #1

--- database.js
+++ database.js
@@ -5,9 +5,9 @@ class Database {
     constructor(user, password, database) {
         this._pool = mysql.createPool({
             host:     MysqlServerHostname,
-            user:     user     | process.argv['MYSQL_USER'],
-            password: password | process.argv['MYSQL_PASSWORD'],
-            database: database | process.argv['MYSQL_DATABASE']
+            user:     user     ,
+            password: password ,
+            database: database 
         })
     }
 
