07a6db2120ac052f61e801d50b15b3250eb49b0f
jiniux <alessandro.gugliotta.02@gmail.com>
2020-09-22 21:17:08 +0200

    Revert "Migration to MariaDB"
    This reverts commit e41ee9cca4dc7fd8a919ba1d03a8f2f12ba0d368.

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 module.exports = mysql.createPool({
-    host: 'piwabot-mariadb',
-    user: process.env['MARIADB_USER'],
-    password: process.env['MARIADB_PASSWORD'],
-    database: process.env['MARIADB_DATABASE'],
+    host: 'piwabot-mysql',
+    user: process.env['MYSQL_USER'],
+    password: process.env['MYSQL_PASSWORD'],
+    database: process.env['MYSQL_DATABASE'],
 
     supportBigNumbers: true,
     bigNumberStrings : true
